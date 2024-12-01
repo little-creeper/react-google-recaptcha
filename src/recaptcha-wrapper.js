38fe9b3b943286fbefa1e7804af0274124607873
sectly <77864650+little-creeper@users.noreply.github.com>
2024-12-01 13:07:34 +0800

    Update recaptcha-wrapper.js

--- src/recaptcha-wrapper.js
+++ src/recaptcha-wrapper.js
@@ -4,21 +4,13 @@ import makeAsyncScriptLoader from "react-async-script";
 const callbackName = "onloadcallback";
 const globalName = "grecaptcha";
 
-function getOptions() {
-  return (typeof window !== "undefined" && window.recaptchaOptions) || {};
-}
-
 function getURL() {
-  const dynamicOptions = getOptions();
-  const hostname = dynamicOptions.useRecaptchaNet ? "recaptcha.net" : "www.google.com";
-  if (dynamicOptions.enterprise) {
-    return `https://${hostname}/recaptcha/enterprise.js?onload=${callbackName}&render=explicit`;
-  }
+  const hostname = "recaptcha.net";
   return `https://${hostname}/recaptcha/api.js?onload=${callbackName}&render=explicit`;
 }
 
 export default makeAsyncScriptLoader(getURL, {
   callbackName,
   globalName,
-  attributes: getOptions().nonce ? { nonce: getOptions().nonce } : {},
+  attributes: {},
 })(ReCAPTCHA);
